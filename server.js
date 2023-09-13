c57eae146b44b60c63542a9a19fdcaa3866f1faf
Nweekley84 <nweekley27@gmail.com>
2023-09-13 16:28:27 -0700

    added start script to package.json, refactored more server.js

--- server.js
+++ server.js
@@ -1,23 +1,82 @@
-// All dependencies
-const express = require('express');
-
-// Use express
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const uniqid = require("uniqid");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Create the environment variable port
-const PORT = process.env.PORT || 3001;
 
-// Express will make a route per file in the 'public' folder and give it a '/'
-app.use(express.static('public'));
-// Express handels the data parser, middle wear req.body
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("Develop/public"));
 
-// Location of the route files
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
-// Starts the server *hopefully*
-app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "Develop/public/index.html"))
+);
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
+);
+app.get("/api/notes", function (req, res) {
+  fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+    var jsonData = JSON.parse(data);
+    res.json(jsonData);
+  });
 });
+
+const readThenAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      createNewNote(file, parsedData);
+    }
+  });
+};
+
+const createNewNote = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title: title,
+      text: text,
+      id: uniqid(),
+    };
+
+    readThenAppend(newNote, "Develop/db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting new note");
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  let id = req.params.id;
+  let parsedData;
+  fs.readFile("Develop/db/db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      parsedData = JSON.parse(data);
+      const filterData = parsedData.filter((note) => note.id !== id);
+      createNewNote("Develop/db/db.json", filterData);
+    }
+  });
+  res.send(`Deleted note with ${req.params.id}`);
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
